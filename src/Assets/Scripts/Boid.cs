11e347b1aafc04a0d4842dafa896bef8cb110ac7
BaptisteMarechaux <baptiste.marechaux@gmail.com>
2016-01-27 06:28:56 +0100

    structure

---
+++ src/Assets/Scripts/Boid.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boid {
+
+    public  Vector3 position;
+    public Vector3 velocity;
+
+    public Boid()
+    {
+        this.position = new Vector3();
+        this.velocity = new Vector3();
+        
+    }
+    ~Boid() { }
+
+}
---
+++ src/Assets/Scripts/BoidsManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoidsManager : MonoBehaviour {
+
+    Boid[] boids;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void InitializePositions()
+    {
+        for(int i=0;i<boids.Length;i++)
+        {
+            boids[i] = new Boid();
+        }
+    }
+
+    void MoveBoids()
+    {
+        for (int i = 0; i < boids.Length; i++)
+        {
+            boids[i].velocity = boids[i].velocity + MoveToCenter(boids[i]) + GetCloser(boids[i]) + MatchNeighbours(boids[i]);
+
+            boids[i].position = boids[i].position + boids[i].velocity;
+        }
+    }
+
+    Vector3 MoveToCenter(Boid b)
+    {
+        return new Vector3();
+    }
+
+    Vector3 GetCloser(Boid b)
+    {
+        return new Vector3();
+    }
+    
+    Vector3 MatchNeighbours(Boid b)
+    {
+        return new Vector3();
+    }
+}
